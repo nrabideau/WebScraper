3b196d242c20318c1346807350931c49aab2dc8b
Web-Cam <32422075+Web-Cam@users.noreply.github.com>
2020-02-06 10:41:48 -0500

    Update BusinessMapInfo.py

--- BusinessMapInfo.py
+++ BusinessMapInfo.py
@@ -2,25 +2,24 @@ import googlemaps
 from datetime import datetime
 import json
 import keyfile
-# Key to get goolemaps data
+# Key to get goolemaps data. To change API key modify keys.yaml file 
 key = (keyfile.getKey('googlemaps'))
 
 
 # Location here will give us the general location of the business.
 def getAddress(company_name, location):
-    # Place your API key here
     gmaps = googlemaps.Client(key=key)
 
     # We make the location bias only FL since that is our scope here.
     result_list = gmaps.find_place(company_name+location, 'textquery', fields=[
                                    'formatted_address', 'place_id', 'geometry'], location_bias='rectangle:31.16816,-88.79009|25.02837,-79.40776')
 
-    # Now we can return the address.
+    # Now we can return the address , place id (Used to do another API call for more information),Geometry is LAT/LONG
 
     return(result_list)
-# This will get extra information about the company
 
 
+# This funtion will get extra information about the company
 def getadditionalInfo(ID):
 
     gmaps = googlemaps.Client(key=key)
